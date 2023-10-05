f6f6cb742e789ac2821ef06d32e9c6b2444190f4
m.bedair94@gmail.com <Bedair>
2023-10-05 22:05:18 +0300

    Initial Implementation for IoHwAbs_Light

---
+++ MasterNode/PSoC62_MasterNode/source/IoHwAbs_Light.c
@@ -0,0 +1,95 @@
+/**********************************************************************
+* File Name : IoHwAbs_Mic.c
+* 
+* Description : source file for the component responsible for controlling 
+                the Microphone to capture sound
+*
+* Author : Mohamed Bedair
+*
+* Date : 05 Oct 2023
+*
+**********************************************************************/
+
+/**********************************************************************
+*                            Macro Section                            *
+**********************************************************************/
+
+#include "IoHwAbs_Mic.h"
+
+
+#include "cybsp.h"
+#include "cyhal.h"
+#include "cycfg.h"
+
+
+#define LIGHT_PIN           (CYBSP_USER_LED)
+#define PWM_FREQUENCY       (1000000u)
+#define FULL_BRIGHTNESS     (100)
+#define ZERO_BRIGHTNESS     (0)
+
+/**********************************************************************
+*                          Global Variables                           *
+**********************************************************************/
+cyhal_pwm_t pwm_led;
+
+
+/**********************************************************************
+*                           Local Functions                           *
+**********************************************************************/
+
+
+
+/**********************************************************************
+*                          Global Functions                           *
+**********************************************************************/
+/*
+* Funcation Name : IoHwAbs_Light_Init
+* Description    : Used to initialize light module
+* Parameters     : NA
+* Return         : NA
+*/
+void IoHwAbs_Light_Init(void)
+{
+    /* Initialize a PWM resource for driving an LED. */
+    cyhal_pwm_init(&pwm_led, LIGHT_PIN, NULL);
+    cyhal_pwm_set_duty_cycle(&pwm_led, ZERO_BRIGHTNESS,PWM_FREQUENCY);
+    cyhal_pwm_start(&pwm_led);
+}   
+
+
+/*
+* Funcation Name : IoHwAbs_Light_On
+* Description    :Set the Light State to ON 
+* Parameters     : NA
+* Return         : NA
+*/
+void IoHwAbs_Light_On(void)
+{
+    cyhal_pwm_set_duty_cycle(&pwm_led, FULL_BRIGHTNESS, PWM_FREQUENCY);
+}
+
+
+/*
+* Funcation Name : IoHwAbs_Light_Off
+* Description    : Set the Light State to OFF
+* Parameters     : NA
+* Return         : NA
+*/
+void IoHwAbs_Light_Off(void)
+{
+    cyhal_pwm_set_duty_cycle(&pwm_led, ZERO_BRIGHTNESS, PWM_FREQUENCY);
+}
+
+
+/*
+* Funcation Name : IoHwAbs_Light_Set_Brightness
+* Description    : Used to set the brightness value
+* Parameters     : NA
+* Return         : NA
+*/
+void IoHwAbs_Light_Set_Brightness(uint8_t brightness)
+{
+    cyhal_pwm_set_duty_cycle(&pwm_led, brightness ,PWM_FREQUENCY);
+}
+
+
---
+++ MasterNode/PSoC62_MasterNode/source/IoHwAbs_Light.h
@@ -0,0 +1,57 @@
+/**********************************************************************
+* File Name : IoHwAbs_Light.h
+* 
+* Description : header file for the component responsible for controlling 
+                the light
+*
+* Author : Mohamed Bedair
+*
+* Date : 05 Oct 2023
+*
+**********************************************************************/
+
+
+#ifndef IOHWABS_LIGHT_H_
+#define IOHWABS_LIGHT_H_
+
+#include <stdint.h>
+
+
+/*
+* Funcation Name : IoHwAbs_Light_Init
+* Description    : Used to initialize light module
+* Parameters     : NA
+* Return         : NA
+*/
+void IoHwAbs_Light_Init(void);
+
+
+/*
+* Funcation Name : IoHwAbs_Light_On
+* Description    :Set the Light State to ON 
+* Parameters     : NA
+* Return         : NA
+*/
+void IoHwAbs_Light_On(void);
+
+
+/*
+* Funcation Name : IoHwAbs_Light_Off
+* Description    : Set the Light State to OFF
+* Parameters     : NA
+* Return         : NA
+*/
+void IoHwAbs_Light_Off(void);
+
+
+/*
+* Funcation Name : IoHwAbs_Light_Set_Brightness
+* Description    : Used to set the brightness value
+* Parameters     : NA
+* Return         : NA
+*/
+void IoHwAbs_Light_Set_Brightness(uint8_t brightness);
+
+
+
+#endif
